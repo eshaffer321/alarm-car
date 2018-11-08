28ee01d0fc3a4be8871c025ce355fec8b03674e6
Peter <pkabai@csumb.edu>
2018-11-08 12:45:30 -0800

    Adds our code

---
+++ Our Code/Other/sketch_nov01c/sketch_nov01c.ino
@@ -0,0 +1,39 @@
+int piezoPin = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+tone(piezoPin, 100, 100);
+delay(100);
+tone(piezoPin, 400, 100);
+delay(100);
+tone(piezoPin, 900, 100);
+delay(100);
+tone(piezoPin, 1300, 100);
+delay(100);
+tone(piezoPin, 1600, 100);
+delay(100);
+tone(piezoPin, 1900, 100);
+delay(100);
+tone(piezoPin, 1200, 100);
+delay(100);
+tone(piezoPin, 1500, 100);
+delay(100);
+tone(piezoPin, 1800, 100);
+delay(100);
+tone(piezoPin, 2100, 100);
+delay(100);
+tone(piezoPin, 2400, 100);
+delay(100);
+tone(piezoPin, 2700, 100);
+delay(100);
+tone(piezoPin, 3000, 100);
+delay(100);
+tone(piezoPin, 3300, 100);
+delay(100);
+
+}
